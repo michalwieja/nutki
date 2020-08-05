822756113f253bcf9f141e7441a4e53b53ec7ae5
mwConceptX <mwieja@yahoo.com>
2020-08-05 13:42:07 +0200

    Updates

---
+++ precache-manifest.9bc7d0149f8f15bb839c7a2b6c6e6c7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a90cb08dccdc6c5069006c6ac3f591b",
+    "url": "/nutki/index.html"
+  },
+  {
+    "revision": "c6afbf3c8d7a7cb48708",
+    "url": "/nutki/static/css/main.24f1d3fc.chunk.css"
+  },
+  {
+    "revision": "6a15a838c8a2595e216d",
+    "url": "/nutki/static/js/2.41dec0ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nutki/static/js/2.41dec0ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6afbf3c8d7a7cb48708",
+    "url": "/nutki/static/js/main.c7721947.chunk.js"
+  },
+  {
+    "revision": "ceccbf6551fafdcbf9f2",
+    "url": "/nutki/static/js/runtime-main.7c503f79.js"
+  },
+  {
+    "revision": "180d59161b94b79266f187b2c0d9c9b3",
+    "url": "/nutki/static/media/1.180d5916.jpg"
+  },
+  {
+    "revision": "f7c5bdb2699119cf32420fbfdd746399",
+    "url": "/nutki/static/media/2.f7c5bdb2.jpg"
+  },
+  {
+    "revision": "86f7b7d8af21e2b8998b01d7b3b3517f",
+    "url": "/nutki/static/media/3.86f7b7d8.jpg"
+  }
+]);
